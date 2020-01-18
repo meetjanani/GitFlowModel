70dc194eed710ff9cc12b3646d47be2d0403788d
meetjanani <meetjanani47@gmail.com>
2020-01-18 13:35:28 +0530

    // Branch feature 3 Commit 1

--- app/src/main/java/in/m/gitflowmodel/MainActivity.kt
+++ app/src/main/java/in/m/gitflowmodel/MainActivity.kt
@@ -8,5 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Branch feature 3 Commit 1
     }
 }
